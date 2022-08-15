c6f606365d9dd736ebdc224829a1568f45f7b2dd
Quema <105044971+Quema100@users.noreply.github.com>
2022-08-15 15:24:44 +0900

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# youtube-block-chrome-extensions
-유튜브 차단 확장프로그램
---
+++ index.js
@@ -0,0 +1,8 @@
+if(window.location.href.startsWith("https://www.youtube.com/")){
+    window.open('','_self').close(); 
+    if(window.location.href="https://quema100.github.io/no-youtube/"){  
+        window.open('','_self').close();
+    }   
+}
+
+    
