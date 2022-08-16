f74651e9cac108f5070160e541d1917eea85c171
Quema <105044971+Quema100@users.noreply.github.com>
2022-08-16 13:28:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-if(window.location.href.startsWith("https://www.youtube.com/")){
-    window.open('','_self').close(); 
-    if(window.location.href="https://quema100.github.io/no-youtube/"){  
-        window.open('','_self').close();
+if(window.location.href.startsWith("https://www.youtube.com/")){ 
+    if(window.location.href="https://quema100.github.io/no-youtube/"){ 
     }   
 }
 
