820bba7fa5ae7225958a110ba5635b7e6180213b
Quema <105044971+Quema100@users.noreply.github.com>
2022-08-18 15:24:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if(window.location.href.startsWith("https://www.youtube.com/")){ 
-    if(window.location.href="https://quema100.github.io/no-youtube/"){ 
+    if ( 창 . 위치 . href = "https://quema100.github.io/block-site/" ){
     }   
 }
 
