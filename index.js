a680606af6f7855ade810fefc96c878594e82ce7
Quema <105044971+Quema100@users.noreply.github.com>
2022-08-19 14:13:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+history.pushState(null, null, window.location.href);
+
+    window.onpopstate = function () {
+
+        history.go(1);
+
+};
 if(window.location.href.startsWith("https://www.youtube.com/")){ 
-    if ( 창 . 위치 . href = "https://quema100.github.io/block-site/" ){
+    if ( 창 . 위치 . replace ( "https://quema100.github.io/block-site/") ){
     }   
 }
 
