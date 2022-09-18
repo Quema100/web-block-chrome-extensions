f09d2ff5c154cbde30a86511d88e13e81fafbae1
Quema <105044971+Quema100@users.noreply.github.com>
2022-09-18 16:28:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-if(window.location.href.startsWith("https://www.youtube.com/")){
+if(window.location.href.startsWith("website")){
   window.stop(true)
 }
-if(window.location.href.startsWith("https://www.youtube.com/")){ 
+if(window.location.href.startsWith("website")){ 
   var 경고 = confirm("응답이 없습니다")
   if (경고){
     window.history.back()
